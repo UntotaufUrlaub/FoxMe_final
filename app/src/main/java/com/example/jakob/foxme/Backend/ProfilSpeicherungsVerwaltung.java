839d9bcd73baa6cf480cd6a2d4fbd2efa60ef375
Jakob <sturmjakob@yahoo.de>
2015-06-18 16:43:23 +0200

    ProfilSpeicherverwaltung: kann einen String speichern und auslesen; benötigt den Context als Construktor eingabe fragment_second: hab rausgefunden wie man n text in den eingabe feldern anzeigt

---
+++ app/src/main/java/com/example/jakob/foxme/Backend/ProfilSpeicherungsVerwaltung.java
@@ -0,0 +1,66 @@
+package com.example.jakob.foxme.Backend;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Created by Jakob on 18.06.15.
+ */
+public class ProfilSpeicherungsVerwaltung {
+    private Context context;
+
+    public ProfilSpeicherungsVerwaltung(Context c){
+        context=c;
+
+    }
+
+    public void save(String eingabe){
+        try {
+            FileOutputStream fos = context.openFileOutput("Profil.txt",Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+            try {
+                osw.write(eingabe);
+                osw.flush();
+                osw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String load(){
+        String ausgabe="";
+        try {
+            FileInputStream fis = context.openFileInput("Profil.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            char[] data =new char [100];
+            int size;
+
+            try {
+                while((size=isr.read(data))>0){
+                    String read_data= String.copyValueOf(data,0,size);
+                    ausgabe+=read_data;
+                    data=new char[100];
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return ausgabe;
+    }
+}
